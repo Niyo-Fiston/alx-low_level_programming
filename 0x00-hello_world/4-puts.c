263dbe205bb8392fbfbe11a3d53f2fe603eceb1c
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-26 07:26:35 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/*this is a program to print a message */
-
+/**
+ * main - print the string in the puts function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
