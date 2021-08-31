d686884c7470d83215d82ce493997f204841a9fd
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-31 11:21:54 -0700

    display time of the whole day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - this function prints minutes of  the day
+ *
+ * Return: no value
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
