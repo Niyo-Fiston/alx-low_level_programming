c40847c9e3a9d5063cc5e2e5a0228d4c0c0b6fff
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-31 09:11:41 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet
+ *
+ * Descriptiom:this function print 10 times lower case
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= z; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
+
