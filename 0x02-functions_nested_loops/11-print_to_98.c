80b8c03195102d3e58ee7d470b5a29d28249a3b4
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-31 12:15:41 -0700

    print numbers to rich 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,19 +12,15 @@ void print_to_98(int n)
 	{
 		for (; n < 98; n++)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
 	}
 	else
 	{
 		for (; n > 98; n--)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
