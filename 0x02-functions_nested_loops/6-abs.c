0ab68be9982234fc6ce64607e06f5ac4d11735df
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-31 10:14:35 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - function to return absolute value
+ * @c: int for argument for function
+ * Return: number
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (-c);
+	}
+}
