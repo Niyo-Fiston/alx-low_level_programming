ce8b4d8ce1b1c9a47e6236051193f7bf6b5741b2
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-31 09:27:42 -0700

    return value

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - print if aphabet is lowercase
+ * @c: is the interger that our function will use
+ * Return: 1 0r 0
+ */
+it _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+
+	}
+}
