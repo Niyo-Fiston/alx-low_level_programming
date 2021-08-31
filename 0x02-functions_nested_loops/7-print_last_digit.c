60e50615e9517ae12b57825d6ba6ccda4a7c4e36
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-31 10:22:35 -0700

    return last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - this function print last digit of number
+ * @c: int for argument of function
+ *
+ * Return: number
+ */
+
+int print_last_digit(int c)
+{
+	int n;
+
+	n = c % 10;
+
+	return (n);
+}
