4bec88d4d24d0e40819ab4031bd0fe87233de721
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-31 11:54:29 -0700

    print to ward a 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_to_98 - this function print from n to 98
+ * @n: variable of a random number
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
