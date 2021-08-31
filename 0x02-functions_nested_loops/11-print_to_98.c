01689ab361da0c6a9216a7721156dfda7655c8a1
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-08-31 12:09:21 -0700

    print to rich 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			_putchar(n);
 			_putchar(',');
@@ -19,7 +19,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			_putchar(n);
 			_putchar(',');
@@ -27,4 +27,5 @@ void print_to_98(int n)
 		}
 	}
 	_putchar('\n');
+	_putchar(n);
 }
