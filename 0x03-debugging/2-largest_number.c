3a08b59de06c0e0e8d6a3a10a8cee85cac660b1e
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-09-01 12:36:12 -0700

    debug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,21 +11,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
-		break;
 	}
-	else if (b >= a && a >= c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
-		break;
 	}
 	else
 	{
 		largest = c;
 	}
-	printf("\n");
 
 	return (largest);
 }
