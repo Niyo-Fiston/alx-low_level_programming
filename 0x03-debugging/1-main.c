e55b1e0ebd9f18a1550e0d92b7966fd53f21a082
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-09-01 10:03:24 -0700

    debug task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,7 +14,9 @@ int main(void)
 
 	while (i < 10)
 	{
-		putchar(i); /* missed increment below causes infinite loop */
+		/**
+		* putchar(i);
+		*/
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
