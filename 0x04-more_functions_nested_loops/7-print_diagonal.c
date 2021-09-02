17b175fbfa6119e52a66a77345ec64b39db479ba
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-09-02 04:06:50 -0700

    diagonal line fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,21 +8,16 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+		_putchar('\n');
 	for (i = 0; i <= n; i++)
 	{
-		if (n <= 0)
+		for (j = 0; j < i; j++)
 		{
-			_putchar('\n');
+			_putchar(' ');
 		}
-		else
-		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
