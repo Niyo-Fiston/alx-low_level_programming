1df799b81551829e34b1be8a482f69ef2373735a
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-09-03 12:51:25 -0700

    print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,12 @@
  */
 void print_number(int n)
 {
-	int i, j;
-
-	i = n / 10;
-	j = n % 10;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 	if (n / 10)
-		print_number(i`);
-	_putchar(j);
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
