388da0bb749ffb80c7ac61b04a59f3ddaa489ae5
NIYOMUTONI Fiston <niyomutonifiston@gmail.com>
2021-09-09 06:29:05 -0700

    quick fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,6 +5,8 @@
  * @n2: second string of ints
  * @r: result string
  * @size_r: size of result string
+ *
+ * Return: value of r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
